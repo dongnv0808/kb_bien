8fd0fb29c9f1ee7fb877601c22057ab9249f6313
dongnv0808 <yamaraja0808@gmail.com>
2022-05-05 16:18:56 +0700

    kbbien

---
+++ kb_bien.js
@@ -0,0 +1,19 @@
+let a = 20;
+let b = 20.5;
+let c = true;
+let d = 'Hà Nội'
+
+let width = 20;
+let height = 30;
+let area = width*height;
+
+document.write("a=" +a);
+document.write('<br/>');
+document.write("b=" +b);
+document.write('<br/>');
+document.write("c=" +c);
+document.write('<br/>');
+document.write("d=" +d);
+document.write('<br/>');
+
+document.write("area=" + area);
